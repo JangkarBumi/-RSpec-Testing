9c4e98054145a3af109273fc7f2945128fee2da7
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-04-01 17:19:47 +0300

    added my_any test

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -8,4 +8,14 @@ describe Enumerable do
       expect(arr.my_select { |i| i > 3 }).to eq([8, 7, 4, 5, 6])
     end
   end
+
+  describe '#my_any?' do
+    it 'Returns boolean true if condition is passed' do
+      expect(arr.my_any? { |i| i >= 5 }).to eq(true)
+      expect(arr.my_any? { |i| i > 8 }).to eq(false)
+      expect(arr.my_any? { |i| i }).to eq(true)
+      expect(arr.my_any? { |i| i == 0 }).to eq(false)
+      expect(arr.my_any? { |i| i < 2 }).to eq(true)
+    end
+  end
 end
