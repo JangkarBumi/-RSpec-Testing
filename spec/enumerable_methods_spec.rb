d861d96f7ca4681674dd3b236edbc258228cc833
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-04-01 19:24:26 +0300

    Added my_count test

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -1,16 +1,10 @@
 require './lib/enumerable.rb'
 
-describe Enumerable do
+RSpec.describe Enumerable do
   let(:arr) { [1, 2, 3, 8, 7, 4, 5, 6] }
 
-  describe '#my_select' do
-    it 'Returns array with elements bigger than 3' do
-      expect(arr.my_select { |i| i > 3 }).to eq([8, 7, 4, 5, 6])
-    end
-  end
-
   describe '#my_any?' do
-    it 'Returns boolean true if condition is passed' do
+    it 'Returns boolean if condition is passed' do
       expect(arr.my_any? { |i| i >= 5 }).to eq(true)
       expect(arr.my_any? { |i| i > 8 }).to eq(false)
       expect(arr.my_any? { |i| i }).to eq(true)
@@ -20,12 +14,27 @@ describe Enumerable do
   end
 
   describe '#my_none?' do
-    it 'Returns boolean false if condition is passed vise-versa' do
+    it 'Returns boolean if condition is passed' do
       expect(arr.my_none? { |i| i >= 5 }).to eq(false)
       expect(arr.my_none? { |i| i > 8 }).to eq(true)
       expect(arr.my_none? { |i| i }).to eq(true)
       expect(arr.my_none? { |i| i == 0 }).to eq(true)
       expect(arr.my_none? { |i| i < 2 }).to eq(false)
+    end
+  end
+end
+
+RSpec.describe Enumerable do
+  let(:arr) { [1, 2, 3, 1, 7, 2, 5, 1] }
+
+  describe '#my_count' do
+    it ' If an argument is given, counts the number of elements which equals'\
+       ' to argument. If a block is given, counts the number of elements'\
+       ' yielding a true value. ' do
+      expect(arr.my_count(1)).to eq(3)
+      expect(arr.my_count).to eq(arr.length)
+      expect(arr.my_count(4)).to eq(0)
+      expect(arr.my_count(&:even?)).to eq(2)
     end
   end
 end
